e3e922d080319b4548be9b6b8a2421238a85f8dc
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-26 15:16:42 +0200

    add sass files,img and comics.php for data, create home structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,91 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('header-content')
+<header>
 
-    <title>Laravel</title>
+    <nav class="p-0 navbar navbar-expand-lg bg-body-white my_nav">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">characters</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">comics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">tv</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">games</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">collectibiles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">videos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">fans</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">news</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">shop</a>
+                    </li>
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-    <!-- Styles -->
-    @vite('resources/js/app.js')
+</header>
 
-</head>
+@endsection
 
-<body>
+@section('main-content')
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h1>
+                ciao
+            </h1>
+        </div>
+        <div class="col-6">
+            <h1>
+                ciao
+            </h1>
+        </div>
+    </div>
+</div>
 
-    <h1>ciao</h1>
-</body>
+@endsection
 
-</html>
+@section('footer-content')
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h1>test</h1>
+            </div>
+            <div class="col-4">
+                <h1>test</h1>
+            </div>
+            <div class="col-4">
+                <h1>test</h1>
+            </div>
+        </div>
+    </div>
+</footer>
+
+@endsection
