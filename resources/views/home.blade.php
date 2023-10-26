e3a336eb448b47583c9b26c809229f06b5f17904
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-26 15:54:00 +0200

    add header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,20 @@
 @section('header-content')
 <header>
 
+    <div class="header_top container-fluid">
+        <div class="row justify-content-end">
+            <div class="col-2">
+                <p>dc powered visa</p>
+            </div>
+            <div class="col-2 me-5">
+                <p>additional dc sites</p>
+            </div>
+        </div>
+    </div>
     <nav class="p-0 navbar navbar-expand-lg bg-body-white my_nav">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="/../img/dc-logo.png" alt="">
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
